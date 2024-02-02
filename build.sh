0a79e4ad3c9b768aa6939fb5c4fb2daf3fa9cb93
Max Cohen <max.zagouri@pm.me>
2024-02-02 13:31:41 +0100

    Define package name at the top

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
+NAME=aciah
+
 # Define package full name and version
 VERSION=$(cat ./VERSION)
-PACKAGE_NAME="aciah_$VERSION"
+PACKAGE_NAME="$NAME-$VERSION"
 # Create temporary directory
 mkdir -p /tmp/$PACKAGE_NAME
 cp -r ./* /tmp/$PACKAGE_NAME
 mv /tmp/$PACKAGE_NAME .
 # Create tarball
-tar cfz "aciah_$VERSION.tar.gz" $PACKAGE_NAME
+tar cfz "$PACKAGE_NAME.tar.gz" $PACKAGE_NAME
 # Remove temporary directory
 rm -r $PACKAGE_NAME
