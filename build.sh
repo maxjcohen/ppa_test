f09988c7021268d9a170d257a343a99d8b0772f8
Max Cohen <max.zagouri@pm.me>
2024-02-02 12:13:59 +0100

    Add top level directory to tarball build

--- build.sh
+++ build.sh
@@ -1,6 +1,11 @@
+# Define package full name and version
 VERSION=$(cat ./VERSION)
-tar cfz "aciah_$VERSION.tar.gz" \
-    src/* \
-    Makefile \
-    VERSION \
-    README.md
+PACKAGE_NAME="aciah_$VERSION"
+# Create temporary directory
+mkdir -p /tmp/$PACKAGE_NAME
+cp -r ./* /tmp/$PACKAGE_NAME
+mv /tmp/$PACKAGE_NAME .
+# Create tarball
+tar cfz "aciah_$VERSION.tar.gz" $PACKAGE_NAME
+# Remove temporary directory
+rm -r $PACKAGE_NAME
